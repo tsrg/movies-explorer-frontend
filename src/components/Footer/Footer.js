6a0de7a2e134c14adee6ced2e7aaa02ed7a5856a
tsrg <tsg@tmedia.fm>
2021-08-25 02:18:05 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className="footer__underline">
         <p className="footer__copyright">&#169; 2020</p>
         <ul className="footer__links">
-          <a className="footer__link" href="https://praktiku.yandex.ru"><li>Яндекс.Практикум</li></a>
+          <a className="footer__link" href="https://praktikum.yandex.ru"><li>Яндекс.Практикум</li></a>
           <a className="footer__link" href="https://github.com/"><li>Github</li></a>
           <a className="footer__link" href="https://www.facebook.com/"><li>Facebook</li></a>
         </ul>
